c6dfdf1e6c2782354c20a0d71edcfecced80cf59
Ilya <iliya5376@mail.com>
2017-09-30 14:34:30 +0300

    feature(league.py) add changing of table results

--- league.py
+++ league.py
@@ -6,38 +6,80 @@ TEAMS_LIST = ['Наполи', 'Ювентус', 'Интер',
               'Кьево', 'Болонья', 'Фиорентина',
               'Дженова']
 
-SEASON_TABLE = []
+SEASON_TABLE = {}
 
-def change_table():
-  pass
+
+def change_table(match_results):
+    masters = match_results["home"]
+    guests = match_results["away"]
+    masters_name = masters["name"]
+    guests_name = guests["name"]
+    masters_goals = masters["goals"]
+    guests_goals = guests["goals"]
+
+    SEASON_TABLE[masters_name]["stat"]["goals"] += masters_goals
+    SEASON_TABLE[masters_name]["stat"]["goals_conceded"] += guests_goals
+    SEASON_TABLE[guests_name]["stat"]["goals"] += guests_goals
+    SEASON_TABLE[guests_name]["stat"]["goals_conceded"] += masters_goals
+
+    if masters_goals != guests_goals:
+        if masters_goals > guests_goals:
+            SEASON_TABLE[masters_name]["stat"]["wins"] += 1
+            SEASON_TABLE[guests_name]["stat"]["losses"] += 1
+            SEASON_TABLE[masters_name]["stat"]["points"] += 3
+        else:
+            SEASON_TABLE[masters_name]["stat"]["losses"] += 1
+            SEASON_TABLE[guests_name]["stat"]["wins"] += 1
+            SEASON_TABLE[guests_name]["stat"]["points"] += 3
+    else:
+        SEASON_TABLE[masters_name]["stat"]["points"] += 1
+        SEASON_TABLE[guests_name]["stat"]["points"] += 1
+
+    SEASON_TABLE[masters_name]["stat"]["matches"] += 1
+    SEASON_TABLE[guests_name]["stat"]["matches"] += 1
+
+
+def sort_table():
+    season_list = sorted(list(SEASON_TABLE.values()), key=lambda x: x[
+                         "stat"]["points"], reverse=True)
+    return season_list
+
 
 def simulate_match(masters, guests):
-    masters_team = {"name": masters, "goals": randint(0, 10)}
-    guests_team = {"name": guests, "goals": randint(0, 10)}
+    masters_goals = randint(0, 10)
+    guests_goals = randint(0, 10)
+
+    masters_team = {"name": masters, "goals": masters_goals}
+    guests_team = {"name": guests, "goals": guests_goals}
     match_results = {"home": masters_team, "away": guests_team}
+
     return match_results
 
 
 def print_season_results():
+    season_list = sort_table()
     print("N  Клуб Игры Победы Ничьи Поражения Мячи Очки")
-    for number, team in enumerate(SEASON_TABLE):
-        matches_count = team["matches"]
-        wins = team["wins"]
-        losses = team["losses"]
+    for number, team in enumerate(season_list):
+        matches_count = team["stat"]["matches"]
+        wins = team["stat"]["wins"]
+        losses = team["stat"]["losses"]
         drawns = matches_count - wins - losses
-        goals_count = team["goals"]
-        points = team["points"]
+        goals_count = team["stat"]["goals"]
+        points = team["stat"]["points"]
+
         print("{0} {1} {2} {3} {4} {5} {6} {7}".format(
             number, team["name"], matches_count, wins, drawns, losses, goals_count, points))
 
 
 def _main():
     for number, team in enumerate(TEAMS_LIST):
-        team_point = {"place": number, "name": team, "matches": 0,
-                      "wins": 0, "losses": 0, "goals": 0,
-                      "goals_conceded": 0, "points": 0}
-        SEASON_TABLE.append(team_point)
-    print(simulate_match(TEAMS_LIST[3], TEAMS_LIST[6]))
+        team_statistics = {"place": number, "matches": 0,
+                           "wins": 0, "losses": 0, "goals": 0,
+                           "goals_conceded": 0, "points": 0}
+        team_point = {"name": team, "stat": team_statistics}
+        SEASON_TABLE[team] = team_point
+
+    change_table(simulate_match(TEAMS_LIST[3], TEAMS_LIST[6]))
     print_season_results()
 
 
